73439ba69e3483f411454ea6d3429a48c11933c2
shidur baba <shidurbaba@gmail.com>
2018-06-30 13:23:55 -0400

    README.md

---
+++ src/main/java/com/seleniuminterview/project/com/seleniuminterview/project/App.java
@@ -0,0 +1,13 @@
+package com.seleniuminterview.project.com.seleniuminterview.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
